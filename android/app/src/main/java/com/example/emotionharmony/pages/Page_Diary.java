423bac05c7fa0b952634f6c1a7035fb662ed87ec
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-06-05 14:26:15 -0300

    new diary

--- android/app/src/main/java/com/example/emotionharmony/pages/Page_Diary.java
+++ android/app/src/main/java/com/example/emotionharmony/pages/Page_Diary.java
@@ -15,6 +15,8 @@ import android.content.Intent;
 
 import com.example.emotionharmony.R;
 import com.example.emotionharmony.components.BottomMenuView;
+import com.example.emotionharmony.databinding.ActivityPageDiaryBinding;
+import com.example.emotionharmony.databinding.ActivityPageExerciciesBinding;
 
 public class Page_Diary extends AppCompatActivity {
 
@@ -22,7 +24,19 @@ public class Page_Diary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_page_diary);
+        /*
+         * Configura o layout em tela cheia com barras de navegação ocultas
+         */
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        );
+
+        // Infla o layout da tela com view binding
+        ActivityPageDiaryBinding binding = ActivityPageDiaryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
         // Configura o menu inferior para esta tela, ativando a navegação entre seções
