2d52a16c6a57a947abd51151b5d6849abbcbc891
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-06-12 14:05:57 -0300

    server e android

--- server/src/modules/user/services/updateUser.service.js
+++ server/src/modules/user/services/updateUser.service.js
@@ -4,7 +4,11 @@ const CustomError = require("../../../core/errors/CustomError");
 const bcrypt = require("bcrypt");
 
 module.exports.updateUserService = async (id_user, body) => {
-  const { nome, email, cpf, senha } = body;
+  console.log({ body });
+
+  const { nome, email, cpf, senha, telefones } = body;
+  console.log({ id_user, nome, email, cpf, senha, telefones });
+
   const user = await prisma.tb_users.findUnique({ where: { id_user } });
   if (!user) throw new CustomError("Usuário não encontrado", 404);
 
@@ -20,5 +24,18 @@ module.exports.updateUserService = async (id_user, body) => {
   }
   if (senha) data.password_user = await bcrypt.hash(senha, 10);
 
-  return prisma.tb_users.update({ where: { id_user }, data });
+  telefones.forEach(async (telefone) => {
+    await prisma.tb_phone.update({
+      where: { id_phone: Number(telefone.id_phone) },
+      data: {
+        country_code_phone: 55,
+        area_code_phone: Number(
+          telefone.telefone.replace(/\D/g, "").slice(0, 2)
+        ),
+        phone_number: Number(telefone.telefone.replace(/\D/g, "").slice(2)),
+      },
+    });
+  });
+
+  return await prisma.tb_users.update({ where: { id_user }, data });
 };
